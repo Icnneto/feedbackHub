22e5c69e462ad09c816114003515345f2379c50a
Israel Nunes <israelnetonunes@gmail.com>
2025-12-08 19:57:02 -0300

    feat[services]: implement service layer for users

---
+++ lib/services/user/user-service.ts
@@ -0,0 +1,110 @@
+import * as UsersDB from '@/lib/data-access/users'
+import { UserWithRelations, UserWithStats, ServiceResponse } from '@/lib/types'
+import { User } from "@prisma/client";
+
+export async function getUserById(
+    id: string
+): Promise<ServiceResponse<User>> {
+    try {
+        if (!id) {
+            return { success: false, message: 'User ID is required' }
+        };
+
+        const user = await UsersDB.getUserById(id);
+
+        if (!user) {
+            return { success: false, message: 'User not found' }
+        };
+
+        return {
+            success: true,
+            message: 'User retrieved successfully',
+            data: user
+        }
+    } catch (error) {
+        console.error('Error in getUserById service', error);
+        return {
+            success: false,
+            message: 'Error fetching user',
+            error
+        };
+    }
+};
+
+export async function getUserByEmail(
+    email: string
+): Promise<ServiceResponse<User>> {
+    try {
+        if (!email) {
+            return { success: false, message: 'email is required' }
+        };
+
+        const user = await UsersDB.getUserByEmail(email);
+
+        if (!user) {
+            return { success: false, message: 'User not found' }
+        };
+
+        return {
+            success: true,
+            message: 'User retrieved successfully',
+            data: user
+        }
+    } catch (error) {
+        console.error('Error in getUserByEmail service', error);
+        return {
+            success: false,
+            message: 'Error fetching user',
+            error
+        };
+    }
+};
+
+export async function getUserProfile(
+    id: string
+): Promise<ServiceResponse<UserWithRelations>> {
+    try {
+        if (!id) {
+            return { success: false, message: 'User ID is required' }
+        };
+
+        const user = await UsersDB.getUserProfile(id);
+
+        if (!user) {
+            return { success: false, message: 'User not found' }
+        };
+
+        return {
+            success: true,
+            message: 'User profile retrieved successfully',
+            data: user
+        }
+    } catch (error) {
+        console.error('Error in getUserProfile service', error);
+        return {
+            success: false,
+            message: 'Error fetching user profile',
+            error
+        };
+    }
+};
+
+export async function getUserStats(
+    id: string
+): Promise<ServiceResponse<UserWithStats | null>> {
+    try {
+        const stats = await UsersDB.getUserStats(id);
+        return {
+            success: true,
+            message: 'User stats retrieved successfully',
+            data: stats
+        }
+    } catch (error) {
+        console.error('Error in getUserStats service', error);
+        return {
+            success: false,
+            message: 'Error fetching user stats',
+            error
+        };
+    }
+}; 
