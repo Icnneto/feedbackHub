30588fd9de92b485572f02b354aa6582ee075161
Israel Nunes <israelnetonunes@gmail.com>
2026-01-20 15:18:27 -0300

    fix[ui]: removed incorrect background color

--- app/(public)/signup/_components/signup-form.tsx
+++ app/(public)/signup/_components/signup-form.tsx
@@ -27,7 +27,7 @@ export function SignupForm() {
     }, [state, router])
 
     return (
-        <section className="flex min-h-screen bg-zinc-50 px-4 py-16 md:py-32 dark:bg-transparent">
+        <section className="flex min-h-screen px-4 py-16 md:py-32 dark:bg-transparent">
             <form
                 action={formAction}
                 className="bg-muted m-auto h-fit w-full max-w-sm overflow-hidden rounded-[calc(var(--radius)+.125rem)] border shadow-md shadow-zinc-950/5 dark:[--color-muted:var(--color-zinc-900)]">
